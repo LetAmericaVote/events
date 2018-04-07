8a2338e86316a26fe8dec82fa507f3e197e63d13
deadlybutter <897368+itsjoekent@users.noreply.github.com>
2018-04-07 12:36:51 -0400

    api middleware

--- web/src/middleware/api.js
+++ web/src/middleware/api.js
@@ -1,15 +1,45 @@
 import {
+  selectApiRequestIsPending,
+} from '../selectors';
+import {
   API_REQUEST_INITIATED,
-  apiRequestFailed, apiRequestSucceeded,
+  apiRequestFailed,
+  apiRequestSucceeded,
 } from '../actions';
 
-const routing = store => next => action => {
+const api = store => next => action => {
   if (action.type === API_REQUEST_INITIATED) {
-    // ... makeFetchRequest with options
-    // ... ... dispatch failed/succeeded action
+    const { endpoint, options } = action;
+
+    if (selectApiRequestIsPending(endpoint, store.getState())) {
+      return next(action);
+    }
+
+    fetch(endpoint, options)
+      .then(res => {
+        const json = await res.json();
+
+        if (! json || ! json.body) {
+          store.dispatch(apiRequestFailed(endpoint, 'Api request returned no data.'));
+          return;
+        }
+
+        const { body } = json;
+
+        if (res.status !== 200 || body.error) {
+          store.dispatch(apiRequestFailed(endpoint, body.message || 'Api request failed.'));
+          return;
+        }
+
+        store.dispatch(apiRequestSucceeded(endpoint, body));
+      })
+      .catch(error => {
+        console.error(error);
+        store.dispatch(apiRequestFailed(endpoint, 'Api request failed due to network failure.'));
+      });
   }
 
   return next(action);
 };
 
-export default routing;
+export default api;
