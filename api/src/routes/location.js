17534dce8b19250a6722f53f304ae3c4e9fa1ee0
deadlybutter <897368+itsjoekent@users.noreply.github.com>
2018-04-07 10:39:56 -0400

    Adding test ip to location route

--- api/src/routes/location.js
+++ api/src/routes/location.js
@@ -1,7 +1,10 @@
 const request = require('superagent');
+const { DEVELOPMENT_IP, NODE_ENV } = process.env;
 
 async function getLocationFromIp(req, res) {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const ip = NODE_ENV === 'development' && DEVELOPMENT_IP ? DEVELOPMENT_IP : (
+    req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  );
 
   const locationInfoRes = await request(`http://getcitydetails.geobytes.com/GetCityDetails?fqcn=${ip}`);
   const locationInfo = JSON.parse(locationInfoRes.text);
