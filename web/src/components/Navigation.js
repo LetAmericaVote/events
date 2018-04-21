5d32d1b22992e1de2b17408792202315a8a769c4
deadlybutter <897368+itsjoekent@users.noreply.github.com>
2018-04-21 10:37:23 -0400

    Updating navigation

--- web/src/components/Navigation.js
+++ web/src/components/Navigation.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import Rivet from '../hoc/Rivet';
 import { selectIsAuthenticated } from '../selectors';
 import { Logo } from '../blocks/Icons';
-import Section from '../blocks/Section';
 import InternalLink from '../routing/InternalLink';
 import { CallToActionButton } from '../blocks/Button';
 import {
@@ -15,6 +14,37 @@ import {
   PROFILE_ROUTE,
   HOME_ROUTE,
 } from '../routing/routes';
+
+const FindButton = styled(CallToActionButton)`
+  display: none;
+
+  ${props => props.theme.mobileLarge`
+    display: block;
+  `}
+`;
+
+const NavLink = styled.a`
+  font-size: ${props => props.theme.regularFontSize};
+  font-family: ${props => props.theme.regularFontFamily};
+  text-transform: uppercase;
+  text-decoration: none;
+  cursor: pointer;
+  text-align: center;
+  align-self: center;
+
+  ${props => props.theme.baseMarginLeft}
+  ${props => props.theme.fg.night}
+
+  &:hover {
+    ${props => props.theme.fg.primary}
+  }
+`;
+
+const NavRow = styled(FlexAcrossSpaceBetween)`
+  align-items: center;
+  ${props => props.theme.basePaddingHorizontal}
+  ${props => props.theme.smallPaddingVertical}
+`;
 
 const Navigation = (props) => {
   const { isAuthenticated } = props;
@@ -28,49 +58,22 @@ const Navigation = (props) => {
     </a>
   );
 
-  const FindButton = styled(CallToActionButton)`
-    display: none;
-
-    ${props => props.theme.mobileLarge`
-      display: block;
-    `}
-  `;
-
-  const NavLink = styled.a`
-    font-size: ${props => props.theme.regularFontSize};
-    font-family: ${props => props.theme.regularFontFamily};
-    text-transform: uppercase;
-    text-decoration: none;
-    cursor: pointer;
-    text-align: center;
-    align-self: center;
-
-    ${props => props.theme.baseMarginLeft}
-    ${props => props.theme.fg.night}
-  `;
-
-  const NavRow = styled(FlexAcrossSpaceBetween)`
-    align-items: center;
-  `;
-
   return (
-    <Section>
-      <NavRow>
-        <InternalLink to={HOME_ROUTE}>
-          <HomeButton />
+    <NavRow>
+      <InternalLink to={HOME_ROUTE}>
+        <HomeButton />
+      </InternalLink>
+      <FlexAcross>
+        <InternalLink to={SEARCH_ROUTE}>
+          <FindButton>Find A House Party</FindButton>
         </InternalLink>
-        <FlexAcross>
-          <InternalLink to={SEARCH_ROUTE}>
-            <FindButton>Find A House Party</FindButton>
-          </InternalLink>
-          <InternalLink to={PROFILE_ROUTE}>
-            <NavLink>
-              {isAuthenticated ? "Profile" : "Log in"}
-            </NavLink>
-          </InternalLink>
-        </FlexAcross>
-      </NavRow>
-    </Section>
+        <InternalLink to={PROFILE_ROUTE}>
+          <NavLink>
+            {isAuthenticated ? "Profile" : "Log in"}
+          </NavLink>
+        </InternalLink>
+      </FlexAcross>
+    </NavRow>
   )
 }
 
